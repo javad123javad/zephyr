f4b59fe1121fba29acd3e836e512684080891228
Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
2024-02-20 16:57:12 +0100

    add baremetall

---
+++ samples/drivers/audio/st_dmic/src/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <soc.h>
+#include <stm32wbxx_hal_sai.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(dmic_sample);
+
+int main(void)
+{
+
+	LOG_INF("Exiting");
+	return 0;
+}
