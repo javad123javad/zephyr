b90ff5c99dda6ba7e5caa84d8c8dd157d651b567
Javad Rahimipetroudi <javad321javad@gmail.com>
2025-03-30 18:05:02 +0200

    add template for SI5351
    Signed-off-by: Javad Rahimipetroudi <javad321javad@gmail.com>

---
+++ include/zephyr/drivers/oscilator.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Public Oscilator driver APIs
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_OSCILATOR_H_
+#define ZEPHYR_INCLUDE_DRIVERS_OSCILATOR_H_
+
+/**
+ * @brief Oscilator Interface
+ * @defgroup osilator_interface Oscilator Interface
+ * @since 4.1
+ * @version 1.0.0
+ * @ingroup io_interfaces
+ * @{
+ */
+
+#include <errno.h>
+
+#include <zephyr/types.h>
+#include <zephyr/device.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <zephyr/syscalls/oscilator.h>
+
+
+#endif /*ZEPHYR_INCLUDE_DRIVERS_OSCILATOR_H_*/
+
