936eef21c5c738f76a8e33cdd1c810febfe1a6e4
Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
2026-01-07 18:52:46 +0100

    fix: stashed commit
    Signed-off-by: Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>

--- boards/weact/blackpill_f411ce/board.cmake
+++ boards/weact/blackpill_f411ce/board.cmake
@@ -1,8 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32F411CE" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
