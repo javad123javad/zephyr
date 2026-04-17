695ded2ddfaf5164c99ac1926d6285219e09c53b
Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
2026-04-17 11:32:06 +0200

    stagging commit
    Signed-off-by: Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>

--- boards/weact/stm32g431_core/board.cmake
+++ boards/weact/stm32g431_core/board.cmake
@@ -5,7 +5,9 @@
 #
 
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+board_runner_args(jlink "--device=STM32G431RB" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,11 +4,37 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "zephyr/sys/printk.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <zephyr/net_buf.h>
+NET_BUF_SIMPLE_DEFINE(demo_buff, 64);
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+        net_buf_simple_init(&demo_buff, 0);  // reserve 1 byte headroom
+
+        const uint8_t tx_buf[16] = "HELLO\r\n";
+        uint8_t rx_buf[16] = {0};
+
+        net_buf_simple_add_mem(&demo_buff, tx_buf, sizeof(tx_buf));
+#if 0
+        for(int i = sizeof(buf)-1; i >=0; i--)
+        {
+                printk("%c", buf[i]);
+                net_buf_simple_add_u8(&demo_buff, buf[i]);
+        }
+        for(int i =0; i <net_buf_simple_max_len(&demo_buff); i++)
+        {
+                uint8_t ch = net_buf_simple_remove_u8(&demo_buff);
+                printk("%d\n", ch);
+                rxBuf[i] = ch;
+        }
+
+        printk("Rx Buf: %s\n", rxBuf);
+#endif
+        printk("rx: %s\n",net_buf_simple_remove_mem(&demo_buff, sizeof(tx_buf)));
+
 
 	return 0;
 }
