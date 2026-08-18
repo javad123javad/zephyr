06e5aa1ada4ea2fd09cfd9de20908278c1506eb2
Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
2026-08-18 10:07:47 +0200

    fix: modify device three for MindOS
    Signed-off-by: Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>

--- boards/st/nucleo_n657x0_q/board.cmake
+++ boards/st/nucleo_n657x0_q/board.cmake
@@ -9,7 +9,7 @@ else()
   board_runner_args(stm32cubeprogrammer "--port=swd")
   board_runner_args(stm32cubeprogrammer "--no-reset")
   board_runner_args(stm32cubeprogrammer "--tool-opt= mode=HOTPLUG ap=1")
-  board_runner_args(stm32cubeprogrammer "--extload=MX25UM51245G_STM32N6570-NUCLEO.stldr")
+  board_runner_args(stm32cubeprogrammer "--extload=Template_FSBL_XIP_ExtMemLoader.stldr")
   board_runner_args(stm32cubeprogrammer "--download-address=0x70000000")
 endif()
 
