73cf01e50e0a6bdf866724006732492262d8f377
Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
2026-09-10 07:37:48 +0200

    feat: add xip for n6 (not working)
    Signed-off-by: Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>

---
+++ boards/st/nucleo_n657x0_q/sysbuild.cmake
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2025 STMicroelectronics
+
+if(SB_CONFIG_BOOTLOADER_MCUBOOT)
+  set_target_properties(mcuboot PROPERTIES BOARD nucleo_n657x0_q/stm32n657xx/fsbl)
+endif()
+
